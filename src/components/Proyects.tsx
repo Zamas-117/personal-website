e84c9642e91b16627ff87c74b5946c18ff693865
CesarRodarte117 <al215430@alumnos.uacj.mx>
2024-12-14 21:16:54 -0700

    netyfli-deploy

--- src/components/Proyects.tsx
+++ src/components/Proyects.tsx
@@ -1,6 +1,7 @@
 import "../css/Proyect.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faCode, faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 type Props = {};
 
@@ -13,17 +14,18 @@ const projects = [
       "En este proyecto se construyó toda una aplicación web de E-commerce abarcando inicio de sesión para los usuarios hasta el envío de los productos.",
     link: "#",
     technologies: [
-      "/logos/python.png",
       "/logos/aws.png",
+      "/logos/postgresql.svg",
+      "/logos/google.png",
+      "/logos/python.png",
       "/logos/html5.png",
       "/logos/css.png",
       "/logos/js.png",
-      "/logos/postgresql.svg",
-      "/logos/bootstrap.svg",
-      "/logos/google.png",
       "/logos/paypal.png",
       "/logos/stripe.png",
     ],
+    repoLink: "https://github.com/Zamas-117/Solid-Steel", // Link al repositorio
+    liveLink: "", // Link al proyecto en producción
   },
   {
     id: 2,
@@ -37,6 +39,8 @@ const projects = [
       "/logos/js.png",
       "/logos/bootstrap.svg",
     ],
+    repoLink: "https://github.com/Zamas-117/Koko-wings", // Link al repositorio
+    liveLink: "https://kokowings.netlify.app/", // Link al proyecto en producción
   },
   {
     id: 3,
@@ -52,6 +56,8 @@ const projects = [
       "/logos/js.png",
       "/logos/bootstrap.svg",
     ],
+    repoLink: "https://github.com/Zamas-117/PAGUINAPAULINA", // Link al repositorio
+    liveLink: "https://pausings4u.com/", // Link al proyecto en producción
   },
   {
     id: 4,
@@ -67,6 +73,8 @@ const projects = [
       "/logos/js.png",
       "/logos/sql.png",
     ],
+    repoLink: "https://github.com/Zamas-117/cecytec-cuestionarios", // Link al repositorio
+    liveLink: "https://cecyteccuestionarios.io/", // Link al proyecto en producción
   },
 ];
 
@@ -74,7 +82,7 @@ function Proyects({}: Props) {
   return (
     <div className="proyects">
       <h1 id="proyects">
-        <FontAwesomeIcon icon={faCode} /> Proyects
+        <FontAwesomeIcon icon={faCode} /> Proyectos
       </h1>
 
       <div className="container_proyects">
@@ -102,9 +110,35 @@ function Proyects({}: Props) {
                 ))}
               </div>
 
-              <a href={project.link} className="btn">
-                Ver proyecto
-              </a>
+              {/* Botones */}
+              <div className="button-container">
+                {/* Botón para el repositorio de GitHub */}
+                <a
+                  href={project.repoLink}
+                  className="btn btn-github"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FontAwesomeIcon icon={faGithub} className="github-icon" />{" "}
+                  Ver Repositorio
+                </a>
+
+                {/* Botón para la página en producción */}
+                {project.liveLink && (
+                  <a
+                    href={project.liveLink}
+                    className="btn btn-live"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FontAwesomeIcon
+                      icon={faExternalLinkAlt}
+                      className="live-icon"
+                    />{" "}
+                    Ver Proyecto
+                  </a>
+                )}
+              </div>
             </div>
           </div>
         ))}
